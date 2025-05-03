2337e961947b19071da6f14ae51ed362ff21d447
Aidis <aidiss@gmail.com>
2025-05-03 11:23:47 +0300

    Update

--- examples/web_search_agent.py
+++ examples/web_search_agent.py
@@ -2,6 +2,7 @@ import os
 from pydantic_ai.agent import Agent
 from pydantic_ai.common_tools.tavily import tavily_search_tool
 import dotenv
+from pydantic import BaseModel
 
 dotenv.load_dotenv()
 
@@ -10,12 +11,30 @@ api_key = os.getenv("TAVILY_API_KEY")
 assert api_key is not None
 
 
+class SearchResult(BaseModel):
+    title: str
+    description: str
+
+
+class SearchResults(BaseModel):
+    results: list[SearchResult]
+
+
 agent = Agent(
     "openai:o3-mini",
     tools=[tavily_search_tool(api_key)],
-    system_prompt="Search Tavily for the given query and return the results.",
+    output_type=SearchResults,
+    system_prompt="Search Tavily for the given query",
+    instrument=True,
 )
 
 if __name__ == "__main__":
-    response = agent.run_sync("Find LLM Agent frameworks")
+    import logfire
+
+    logfire.configure()
+    logfire.instrument_httpx()
+    response = agent.run_sync(
+        "Find LLM Agent frameworks."
+        #  Do extra search for each framework you find to get extra details
+    )
     print(response)
